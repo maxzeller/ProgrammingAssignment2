d277b7717a6f26a1b53bef2d2bf18a9cc4ed0876
maxzeller <maxime.zeller@yahoo.fr>
2016-05-16 15:57:38 +0200

    Pushing assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# Creates a special function  to cache or get its argument or its inverse (if already available)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
+# cacheSolve calculates the inverse of the"result-matrix" created with the above 
+# function. It uses cached result if it is available or coputes it if not already available.
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+#Example how to use
+#================
+test<- makeCacheMatrix(matrix(1:4,2, 2)) 
+cacheSolve(test) #calculation performed because not yet cached
+cacheSolve(test) # no calculation performed because result already cached reused
+
